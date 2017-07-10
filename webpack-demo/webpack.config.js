838f34262fab7b6bd1c88c7f5e2496a2336993fa
chenshi <280580216@qq.com>
2017-07-10 18:05:10 +0800

    output-management/

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,17 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/scripts/index.js',
+    vendor: ['react', 'react-dom'],
     output: {
-        filename: "bundle.js",
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            favicon: '../src/images/favicon.ico'
+        })
+    ]
 };
