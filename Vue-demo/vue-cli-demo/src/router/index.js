d52f611416ccc1c3ad2267b634cc233f6cc214ca
chenshi <280580216@qq.com>
2017-07-27 15:09:06 +0800

    commit vue-cli-admin-demo

---
+++ Vue-demo/vue-cli-demo/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const _import = require('./_import_' + process.env.NODE_ENV);
+/* login */
+const Login = _import('login/index');
+const authRedirect = _import('login/authredirect');
+
+/* error page */
+const Err404 = _import('error/404');
+const Err401 = _import('error/401');
+
+Vue.use(Router)
+
+export const constantRouterMap = [{
+  path: '/login',
+  component: Login,
+  hidden: true
+},
+{
+  path: '/authredirect',
+  component: authRedirect,
+  hidden: true
+},
+{
+  path: '/404',
+  component: Err404,
+  hidden: true
+},
+{
+  path: '/401',
+  component: Err401,
+  hidden: true
+}
+]
+
+export default new Router({
+  scrollBehavior: () => ({
+    y: 0
+  }),
+  routes: constantRouterMap
+})
--- Vue-demo/vue-cli-example/src/components/Table.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <el-row>
-    <el-col :span="24">
-      <div class="grid-content bg-purple-dark"></div>
-    </el-col>
-  </el-row>
-</template>
